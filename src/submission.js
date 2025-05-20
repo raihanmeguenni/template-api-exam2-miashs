fb5dec818868b54a511f8673d465859cbb757523
raihanmeguenni <raihan_@hotmail.fr>
2025-05-20 14:17:37 +0200

    Mise à jour de l'API

--- src/submission.js
+++ src/submission.js
@@ -3,12 +3,11 @@ export const submitForReview = async (fastify) => {
     if (!process.env.API_KEY) {
       fastify.log.error(
         'API_KEY is not set. Please set it as an environment variable on render.com to submit your API for review.'
-      );
-      return;
+      )
+      return
     }
 
     try {
-      // Attendre 2 secondes pour s'assurer que le serveur est bien démarré
       await new Promise(resolve => setTimeout(resolve, 2000));
       const response = await fetch(`https://api-ugi2pflmha-ew.a.run.app/group/submissions?apiKey=${process.env.API_KEY}`, {
         method: 'POST',
@@ -18,17 +17,16 @@ export const submitForReview = async (fastify) => {
         },
         body: JSON.stringify({
           apiUrl: process.env.RENDER_EXTERNAL_URL,
-          gitRepo: process.env.RENDER_GIT_REPO_SLUG  // AJOUT : propriété pour l'examen_session2
         }),
-      });
+      })
 
       if (response.ok) {
-        fastify.log.info('API submitted for review, see https://miashs-exam-api.web.app/');
+        fastify.log.info('API submitted for review, see https://miashs-exam-api.web.app/')
       } else {
-        throw new Error(response.status);
+        throw new Error(response.status)
       }
     } catch (error) {
-      fastify.log.error('Error while submitting your API for review:', error);
+      fastify.log.error('Error while submitting your API for review:', error)
     }
   }
-};
+}
